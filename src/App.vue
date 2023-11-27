c7434b82c7d6f45dd85e2aff065305120764cca4
RamenSSS <thin4545@naver.com>
2023-11-28 01:38:08 +0900

    r

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import HeaderBar from './components/HeaderBar.vue'
+</script>
+
+<template>
+  <div id="header">
+    <HeaderBar />
+  </div>
+  <div id="content">
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+</style>
