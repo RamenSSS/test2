b937934b90c3f451f44971158e0f64df595918b1
RamenSSS <thin4545@naver.com>
2023-11-28 01:43:23 +0900

    q

--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <!-- 홈화면 나중에 만들자 -->
-<RouterLink to="/schedule">Schedule</RouterLink>
-<div></div>
-</template>
-
-<script setup>
-import { RouterLink } from 'vue-router';
-</script>
